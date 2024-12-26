3c93d93c2ce22cfaaf4947bcae2325d2defcc340
Ajay70aa <ajay70aa@gmail.com>
2024-12-26 15:19:19 +0530

    image-slider-project added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 // import Accordian from './components/accordian/index'
 // import RandomColor from './components/random-color'
 import StarRating from './components/star-rating'
+import ImageSlider from './components/image-slider'
 
 function App() {
 
@@ -10,7 +11,8 @@ function App() {
       <div>
         {/* <Accordian /> */}
         {/* <RandomColor /> */}
-        <StarRating noOfStars={10}/>
+        {/* <StarRating noOfStars={10}/> */}
+        <ImageSlider url={"https://picsum.photos/v2/list"} page={'1'} limit={'10'}/>
       </div>    
   )
 }
