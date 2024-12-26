3927118d325af111ba00636c9852102a1ef21096
Ajay70aa <ajay70aa@gmail.com>
2024-12-26 10:07:16 +0530

    added star-rating-project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
-import Accordian from './components/accordian/index'
-import RandomColor from './components/random-color'
+// import Accordian from './components/accordian/index'
+// import RandomColor from './components/random-color'
+import StarRating from './components/star-rating'
 
 function App() {
 
   return ( 
       <div>
         {/* <Accordian /> */}
-        <RandomColor />
+        {/* <RandomColor /> */}
+        <StarRating noOfStars={10}/>
       </div>    
   )
 }
